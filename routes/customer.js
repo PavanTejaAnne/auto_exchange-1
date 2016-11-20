659fc055308a1ecf48e0057b689a6d90961536b8
Satyateja Pothuru <satyateja27@gmail.com>
2016-11-19 17:00:29 -0800

    satya: first api

---
+++ routes/customer.js
@@ -0,0 +1,30 @@
+/**
+ * Created by satyateja on 11/19/2016.
+ */
+var express = require('express');
+var mysql = require('./mysql');
+var ejs=require('ejs');
+
+exports.getCustomer = function(req, res){
+    ssn = req.param("ssn");
+    var customer = "select * from Customer where SSN = '"+ssn+"'";
+    mysql.fetchData(customer, function(err, results) {
+            if (err) {
+                throw err;
+            } else {
+                if (results.length > 0) {
+                    console.log(results);
+                    res.send({
+                        "status": 200,
+                        "message:": "login successful!",
+                        "profile": results
+                    });
+                }
+                // render or error
+                else {
+                    res.end('An error occurred');
+                    console.log(err);
+                }
+            }
+    });
+};
---
+++ routes/dealer.js
@@ -0,0 +1,7 @@
+/**
+ * Created by satyateja on 11/19/2016.
+ */
+
+function getCustomer(req, res){
+    var get
+}
