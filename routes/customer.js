26d6163a8e495fd69327a27982ab921cf0640a9b
DhirajGurnani <dhirajgurnani20@gmail.com>
2016-11-28 10:42:05 -0800

    mobile update email fixed

--- routes/customer.js
+++ routes/customer.js
@@ -456,8 +456,11 @@ exports.updateCustomerPhoneNo = function(req, res){
     var ssn = req.query.ssn;
     var primary_mobile = req.query.primary_mobile;
     var secondary_mobile = req.query.secondary_mobile;
-    var primary_sql = "update cus_mobile set mobile_no = '"+ primary_mobile+"' where cus_mobile_ssn = '"+ ssn +"' and mobile_no = '" + primary_mobile + "'";
-    var sec_sql = "update cus_mobile set mobile_no = '"+ primary_mobile+"' where cus_mobile_ssn = '"+ ssn +"' and mobile_no = '" + secondary_mobile + "'";
+    var primary_old_mobile = req.query.primary_old_mobile;
+    var secondary_old_mobile = req.query.secondary_old_mobile;
+
+    var primary_sql = "update cus_mobile set mobile_no = '"+ primary_mobile+"' where cus_mobile_ssn = '"+ ssn +"' and mobile_no = '" + primary_old_mobile + "'";
+    var sec_sql = "update cus_mobile set mobile_no = '"+ primary_mobile+"' where cus_mobile_ssn = '"+ ssn +"' and mobile_no = '" + secondary_old_mobile + "'";
     mysql.fetchData(primary_sql, function(err, results) {
         if (err) {
             throw err;
@@ -481,11 +484,14 @@ exports.updateCustomerPhoneNo = function(req, res){
 
 
 exports.updateCustomerEmail = function(req, res){
-    var ssn = req.query.ssn;
+    var ssn = req.query.SSN;
     var primary_email = req.query.primary_email;
     var secondary_email = req.query.secondary_email;
-    var primary_sql = "update cus_email set email = '"+ primary_email +"' where cus_email_ssn = '"+ ssn +"' and email='"+ primary_email + "'";
-    var sec_sql = "update cus_email set email = '"+ secondary_email +"' where cus_email_ssn = '"+ ssn  +"' and email='"+ secondary_email + "'";
+    var primary_old_email = req.query.primary_old_email;
+    var secondary_old_email = req.query.secondary_old_email;
+
+    var primary_sql = "update cus_email set email = '"+ primary_email +"' where cus_email_ssn = '"+ssn+"' and email = '"+primary_old_email+"'";
+    var sec_sql = "update cus_email set email = '"+ secondary_email +"' where cus_email_ssn = '"+ssn+"' and email = '"+secondary_old_email+"'";
 
     mysql.fetchData(primary_sql, function(err, results) {
         if (err) {
@@ -506,6 +512,7 @@ exports.updateCustomerEmail = function(req, res){
             }
         });
     }
+
 };
 
 exports.deleteCustomerEmail = function(req, res){
